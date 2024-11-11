dd6a204c78414e925e2d35d3d75ab3264b200ecf
HarshilVadgama31 <vadgama.harshil31@gmail.com>
2024-11-12 01:08:17 +0530

    Completed Project 3: Calculator

--- calculator/calculator/Views/KeyView.swift
+++ calculator/calculator/Views/KeyView.swift
@@ -82,7 +82,71 @@ struct KeyView: View {
     }
     
     func didTap(button: Keys){
-        print("Button Tapped")
+        switch button {
+        case .add, .subtract, .multiply, .divide, .equals:
+            if button == .add {
+                self.currentOperation = .add
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .subtract {
+                self.currentOperation = .subtract
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .multiply {
+                self.currentOperation = .multiply
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .divide {
+                self.currentOperation = .divide
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .equals {
+                let runningValue = self.runningNumber
+                let currentValue = Int(self.value) ?? 0
+                
+                switch self.currentOperation {
+                case .add:
+                    self.value = "\(runningValue + currentValue)"
+                case .subtract:
+                    self.value = "\(runningValue - currentValue)"
+                case .multiply:
+                    self.value = "\(runningValue * currentValue)"
+                case .divide:
+                    self.value = "\(runningValue / currentValue)"
+                case .none:
+                    break
+                }
+            }
+            if button != .equals {
+                self.value = "0"
+            }
+        case .clear:
+            self.value = "0"
+            self.runningNumber = 0
+        case .decimal, .negative, .percent:
+            if button == .negative {
+                self.value = "-\(self.value)"
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .percent {
+                self.runningNumber = Int(self.value) ?? 0
+                let currentValue = Double(self.runningNumber)/100
+                self.value = "\(currentValue)"
+            }
+            else if button == .decimal {
+                self.value = "\(self.value).\(self.value)"
+            }
+            
+            break
+        default:
+            let number = button.rawValue
+            if self.value == "0" {
+                value = number
+            }
+            else {
+                self.value = "\(self.value)\(number)"
+            }
+        }
     }
     
 }
